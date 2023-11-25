a76432b36be62361417455968fead7d6eca1a8a0
Ali Belalia <89483298+alibelalia@users.noreply.github.com>
2023-11-25 12:10:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 function checkName() {
     var name = document.getElementById("nameInput").value;
-    var popup = document.getElementById("myModal");
     var popupText = document.getElementById("popup-text");
 
     if (name.trim() !== "") {
         popupText.innerHTML = "Thank you for visiting our website, " + name + "!";
     } else {
         popupText.innerHTML = "Please enter a name.";
-    }
-
-    if (name.toLowerCase() === "ahlam") {
-        popupText.innerHTML = "Hi Ahlam! Ali Belalia loves you.<br>Here's a special message: 'You make every moment special!'";
     }
 
     $('#myModal').modal('show');
